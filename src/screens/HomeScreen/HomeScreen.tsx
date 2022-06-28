195da367e4dfbf083e509d534cd31b6adbacd678
Mohamed <Eng_Mohamed_Sarhan@outlook.com>
2022-06-28 17:26:00 +0200

    Renamed user component

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import React, { FC, memo } from 'react';
 import { View } from 'react-native';
 
-import UserHero from '../../components/UserHero/UserHero';
+import User from '../../components/User/User';
 import { profileImg } from '../../constants/images';
 import { INavigationProp } from '../../constants/types';
 import styles from '../../styles/styles';
@@ -9,7 +9,7 @@ import styles from '../../styles/styles';
 const HomeScreen: FC<INavigationProp> = memo(() => {
   return (
     <View style={styles.screen}>
-      <UserHero name="mohamed khaled" image={profileImg} />
+      <User name="mohamed khaled" image={profileImg} />
     </View>
   );
 });
