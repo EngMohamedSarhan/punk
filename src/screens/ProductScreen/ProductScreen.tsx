28a17d3cb90730ae9e8d49f16efb7f6400862e54
Mohamed <Eng_Mohamed_Sarhan@outlook.com>
2022-07-01 16:45:42 +0200

    Product screen improvements

--- src/screens/ProductScreen/ProductScreen.tsx
+++ src/screens/ProductScreen/ProductScreen.tsx
@@ -1,4 +1,5 @@
-import React, { FC, memo } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { FC, memo, MutableRefObject, useRef } from 'react';
 import { ScrollView, View } from 'react-native';
 
 import ProductAction from '../../components/ProductAction/ProductAction';
@@ -14,46 +15,58 @@ import { palette } from '../../styles/palette';
 import { sizes } from '../../styles/sizes';
 import styles from '../../styles/styles';
 
-const ProductScreen: FC<INavigationProp> = memo(({ navigation }) => (
-  <ProductContext.Consumer>
-    {(context) => {
-      const { image_url, name, description, ebc } = context!.product!;
+const ProductScreen: FC<INavigationProp> = memo(({ navigation }) => {
+  const containerRef = useRef() as MutableRefObject<ScrollView>;
 
-      return (
-        <ScreenLayout>
-          <ProductHeader navigation={navigation} />
-          <ScrollView style={{ backgroundColor: palette.background }}>
-            <ProductImage uri={image_url} />
-            <View style={[{ marginLeft: sizes.lg }, styles.screenPadding]}>
-              <Typography
-                size="4xl"
-                fontWeight="800"
-                marginTop={sizes.lg}
-                fontColor="primaryFont"
-              >
-                {name}
-              </Typography>
-              <View style={[styles.horizontalContainer, styles.spaceBetween]}>
-                <StarRating score={4.7} style={styles.separator} />
-                <ProductQuantity />
+  const handleScrollToTop = () =>
+    containerRef.current?.scrollTo({ y: 0, animated: false });
+
+  useFocusEffect(handleScrollToTop);
+
+  return (
+    <ProductContext.Consumer>
+      {(context) => {
+        const { image_url, name, description, ebc } = context!.product!;
+
+        return (
+          <ScreenLayout>
+            <ProductHeader navigation={navigation} />
+            <ScrollView
+              ref={containerRef}
+              style={{ backgroundColor: palette.background }}
+            >
+              <ProductImage uri={image_url} />
+              <View style={[{ marginLeft: sizes.lg }, styles.screenPadding]}>
+                <Typography
+                  size="4xl"
+                  fontWeight="800"
+                  marginTop={sizes.lg}
+                  fontColor="primaryFont"
+                >
+                  {name}
+                </Typography>
+                <View style={[styles.horizontalContainer, styles.spaceBetween]}>
+                  <StarRating score={4.7} style={styles.separator} />
+                  <ProductQuantity />
+                </View>
+                <Typography fontWeight="800" size="lg" fontColor="primaryFont">
+                  Description
+                </Typography>
+                <Typography
+                  size="sm"
+                  lineHeight={20}
+                  style={styles.miniSeparator}
+                >
+                  {description}
+                </Typography>
+                <ProductAction price={ebc} style={styles.miniSeparator} />
               </View>
-              <Typography fontWeight="800" size="lg" fontColor="primaryFont">
-                Description
-              </Typography>
-              <Typography
-                size="sm"
-                lineHeight={20}
-                style={styles.miniSeparator}
-              >
-                {description}
-              </Typography>
-              <ProductAction price={ebc} style={styles.miniSeparator} />
-            </View>
-          </ScrollView>
-        </ScreenLayout>
-      );
-    }}
-  </ProductContext.Consumer>
-));
+            </ScrollView>
+          </ScreenLayout>
+        );
+      }}
+    </ProductContext.Consumer>
+  );
+});
 
 export default ProductScreen;
