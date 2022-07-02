75d6a509202c84279de808117e6ce3011946b091
Mohamed <Eng_Mohamed_Sarhan@outlook.com>
2022-07-02 13:09:50 +0200

    Created cart item UI

---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,65 @@
+import React, { FC, memo, useContext } from 'react';
+import { Image, View, ViewProps } from 'react-native';
+
+import { ICartItem } from '../../constants/types';
+import CartContext from '../../context/CartContext';
+import { sizes } from '../../styles/sizes';
+import styles from '../../styles/styles';
+import IconButton from '../IconButton/IconButton';
+import {
+  decButtonProps,
+  incButtonProps,
+} from '../ProductQuantity/ProductQuantity';
+import Typography from '../Typography/Typography';
+
+export interface ICartItemProps extends ViewProps {
+  item: ICartItem;
+  index: number;
+  category?: string;
+}
+
+const CartItem: FC<ICartItemProps> = memo(
+  ({ item, index, category = 'Beer', style, ...props }) => {
+    const { name, quantity, image_url } = item;
+    const { handleIncrement, handleDecrement } = useContext(CartContext)!;
+
+    const handleIncPress = () => handleIncrement(index);
+
+    const handleDecPress = () => handleDecrement(index);
+
+    return (
+      <View
+        {...props}
+        style={[styles.horizontalContainer, styles.spaceBetween, style]}
+      >
+        <View style={[styles.flex, { marginRight: sizes.md }]}>
+          <Typography fontColor="primaryFont">{name}</Typography>
+          <Typography size="sm" marginTop={6}>
+            {category}
+          </Typography>
+        </View>
+
+        <View style={styles.horizontalContainer}>
+          <View
+            style={[
+              styles.spaceBetween,
+              styles.center,
+              { marginRight: sizes.sm },
+            ]}
+          >
+            <IconButton {...incButtonProps} onPress={handleIncPress} />
+            <Typography fontColor="black">{quantity}</Typography>
+            <IconButton {...decButtonProps} onPress={handleDecPress} />
+          </View>
+          <Image
+            source={{ uri: image_url }}
+            resizeMode="contain"
+            style={styles.cartImage}
+          />
+        </View>
+      </View>
+    );
+  }
+);
+
+export default CartItem;
